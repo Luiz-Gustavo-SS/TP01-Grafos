a4cf277eff088d966433bab2bbd77a6f36b0c1c1
Luiz-Gustavos-SS <luiz.soares.1310151@sga.pucminas.br>
2022-05-05 15:50:21 -0300

    Adicionando construtor a classe Grafos

--- src/business/Grafo.java
+++ src/business/Grafo.java
@@ -6,7 +6,10 @@ public class Grafo {
 	private int numVertices=0;
 	private int numArestas=0;
 	
-	
+	Grafo(){
+		vertices = new Vertice[1000];
+		arestas = new Aresta[16500];
+	}
 	
 	public Vertice[] getVertices() {
 		return vertices;
